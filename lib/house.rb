b4ccaf8e51a25d3c33d2fe38cefade0d3b622411
Weston Schutt <wtschutt@gmail.com>
2023-05-18 09:40:52 -0600

    Feature: Add House class and .add_room method

---
+++ lib/house.rb
@@ -0,0 +1,13 @@
+ class House
+  attr_reader :price, :address, :rooms
+
+  def initialize(price, address)
+    @price = price.delete("$").to_i
+    @address = address
+    @rooms = []
+  end
+
+  def add_room(room)
+    @rooms << room
+  end
+ end
