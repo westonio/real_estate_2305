b51df27009c7ba49f271deb693bd9a00a5f71416
Weston Schutt <wtschutt@gmail.com>
2023-05-18 09:51:11 -0600

    Test: Add iteration 2 tests

--- spec/house_spec.rb
+++ spec/house_spec.rb
@@ -37,4 +37,46 @@ RSpec.describe House do
       expect(house.rooms).to eq([room_1, room_2])
     end
   end
+
+  describe "Iteration 3" do
+    it 'determines if above market average' do
+      house_1 = House.new("$400000", "123 sugar lane")
+      house_2 = House.new("$550000", "125 sugar lane")
+
+      expect(house_1.above_market_average?).to eq(false)
+      expect(house_2.above_market_average?).to eq(true)
+    end
+
+    it 'lists rooms by category' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+      house.add_room(room_1)
+      house.add_room(room_2)
+      house.add_room(room_3)
+      house.add_room(room_4)
+
+      expect(house.rooms_by_category(:bedroom)).to eq([room_1, room_2])
+      expect(house.rooms_by_category(:living_room)).to eq([room_3])
+      expect(house.rooms_by_category(:basement)).to eq([room_4])
+    end
+
+    it 'calculates areaa of house' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+      room_3 = Room.new(:living_room, 25, '15')
+      room_4 = Room.new(:basement, 30, '41')
+
+      expect(house.area).to eq(1900)
+    end
+
+    it 'provides house details' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(houde.details).to eq({"price" => 400000, "address" => "123 sugar lane"})
+    end
+  end
 end
