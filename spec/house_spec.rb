9ebbba770bd373e104baadcf843a1f2208304cc5
Weston Schutt <wtschutt@gmail.com>
2023-05-18 09:30:51 -0600

    Test: Add tests for House class/ iteration 2

---
+++ spec/house_spec.rb
@@ -0,0 +1,38 @@
+require './lib/room'
+require './lib/house'
+
+RSpec.describe House do
+  describe "Iteration 2" do
+    it 'is an instance of House' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house).to be_an_instance_of(House)
+    end
+
+    it 'has a price' do
+      house = House.new("$400000", "123 sugar lane")
+      
+      expect(house.price).to eq(400000)
+    end
+
+    it 'has an address' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.address).to eq("123 sugar lane")
+    end
+
+    it 'has no rooms to start' do
+      house = House.new("$400000", "123 sugar lane")
+
+      expect(house.rooms).to eq([])
+    end
+
+    it 'can add rooms' do
+      house = House.new("$400000", "123 sugar lane")
+      room_1 = Room.new(:bedroom, 10, '13')
+      room_2 = Room.new(:bedroom, 11, '15')
+
+      expect(house.rooms).to eq([room_1, room_2])
+    end
+  end
+end
